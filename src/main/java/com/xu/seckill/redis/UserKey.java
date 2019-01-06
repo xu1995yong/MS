a35702d9ba014c1b0e0cdf25c9999d838633641c
徐勇 <xu1995yong@hotmail.com>
2019-01-06 21:15:09 +0800

    modify

--- src/main/java/com/xu/seckill/redis/UserKey.java
+++ src/main/java/com/xu/seckill/redis/UserKey.java
@@ -1,15 +1,10 @@
 package com.xu.seckill.redis;
 
-/**
- * Created by jiangyunxiong on 2018/5/21.
- */
 public class UserKey extends BasePrefix {
 
-    public static final int TOKEN_EXPIRE = 3600*24 *2;//默认两天
+    public static final int TOKEN_EXPIRE = 3600 * 24 * 2;//默认两天
 
-    /**
-     * 防止被外面实例化
-     */
+
     private UserKey(int expireSeconds, String prefix) {
         super(expireSeconds, prefix);
     }
@@ -17,7 +12,7 @@ public class UserKey extends BasePrefix {
     /**
      * 需要缓存的字段
      */
-    public static UserKey token = new UserKey(TOKEN_EXPIRE,"token");
+    public static UserKey token = new UserKey(TOKEN_EXPIRE, "token");
     public static UserKey getById = new UserKey(0, "id");
 
 }
